0d6d18452084f280fa7ea5408345f57e6db1fad9
CarlosOrozco88 <CarlosOrozco88@users.noreply.github.com>
2023-05-10 07:54:41 +0000

    Deploying to gh-pages from @ CarlosOrozco88/ui5-extra@0086aa2592b94a22ac6699d894ded92209ce2437 🚀

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -78,7 +78,7 @@ sap.ui.define([
 		 */
 		version: "1.112.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20230509-1611" }
+		buildinfo : { lastchange : "", buildtime : "20230510-0752" }
 	});
 
 	var syncCallBehavior = Configuration.getSyncCallBehavior();
